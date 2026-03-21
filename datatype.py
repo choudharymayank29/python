dc3e2005258eb6cf7825f4b4a9da3ae398b18e7e
choudharymayank29 <mayankror27@gmail.com>
2026-03-21 20:35:44 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,16 @@
+schoolname = "st.xavier"   #string
+Name="petter"    #string
+age = 15
+ismale = True
+weight = 38.35
+
+
+
+print("name of the student is :",Name)
+print("data type of petter is", type(Name))
+
+
+
+age = str(age)
+print(age)
+print("data type of age is",type(age))
