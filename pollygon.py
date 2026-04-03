340a53b1a3c9f47f6576917541f24127f2d9185c
choudharymayank29 <mayankror27@gmail.com>
2026-04-03 20:30:12 +0530

    Add files via upload

---
+++ pollygon.py
@@ -0,0 +1,27 @@
+import turtle
+
+
+
+turtle.Screen().bgcolor('green')
+
+
+pen = turtle.Turtle()
+
+
+
+#TRIANGLE
+
+
+pen.forward(100)
+
+pen.left(120)
+
+pen.forward(100)
+
+pen.left(120)
+
+pen.forward(100)
+
+pen.left(120)
+
+turtle.done()
