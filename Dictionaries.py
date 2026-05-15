220f47f4fcad4522312389e9e8301cf5f86c160e
choudharymayank29 <mayankror27@gmail.com>
2026-05-15 20:36:39 +0530

    Add files via upload

---
+++ Dictionaries.py
@@ -0,0 +1,30 @@
+
+
+mydictionary = {
+      1: 'apple',
+      2: 'ball',
+}
+
+mydictionary = {
+
+      'name': 'john',
+      1:[2,4,3]
+}
+
+student = {
+    'name': 'jack',
+    'age': 26
+}
+
+
+print(student['name'])
+
+
+student['age'] = 27
+print (student)
+
+student['address'] = 'downtown'
+print(student)
+
+student.pop('age')
+print(student)
