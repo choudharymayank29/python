8b5d0ea5fddf857ef90b162a03f023f58e5d7bc6
choudharymayank29 <mayankror27@gmail.com>
2026-05-22 20:44:50 +0530

    Add files via upload

---
+++ phonebook.py
@@ -0,0 +1,29 @@
+import sys
+
+
+def intial_phonebook ():
+    rows, columns = int(input("please enter intial number of contacts")), 5
+
+    phone_book = []
+    print(phone_book)
+    for i in range (rows):
+        print("\nEnter contact %d details in the following order (ONLY):" % (i+1))
+
+        print("NOTE: * indicates mandatory fields")
+
+        print("....................................................................")
+        temp = [] 
+        for j in range(columns):
+
+            if j == 0:
+                if temp [j] ==' 'or temp[j]==' ':
+                    sys.exit("Name is a mandatory field. Process exiting due to blank field...")
+           
+            if j == 1:
+                        temp.append(int(input("enter number*:")))
+
+            if j == 2:
+                        temp.append(int(input("enter number*:")))
+
+                        if temp [j] == ' 'or temp[j] == ' ':
+                              temp[j] = None
