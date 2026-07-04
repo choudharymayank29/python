32a56fffa2997fd4e69bd326cd93011ada667b1b
choudharymayank29 <mayankror27@gmail.com>
2026-07-04 20:52:51 +0530

    Add files via upload

---
+++ operationparttwo.py
@@ -0,0 +1,7 @@
+filewrite = open('codingal.txt' , 'w')
+filewrite.write("i have to change coding txt")
+filewrite.close()
+
+fileappened = open('codingal.txt' , 'a')
+fileappened.write("lets appened , i love volleball")
+fileappened.close
