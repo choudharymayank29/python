bcef906cf968c515d0ebf0aaf1ce3af6a4659025
choudharymayank29 <mayankror27@gmail.com>
2026-09-19 20:46:42 +0530

    Add files via upload

--- gussethenumber.py
+++ gussethenumber.py
@@ -1,28 +1,44 @@
-import rondom
+import random
 attempts_list = []
 def show_score():
-    if len (attempts_list) <= 0:
-        print("there is currently no high score,it's yours for talking!")
+    if len(attempts_list) <= 0:
+        print("There is currently no high score, it's yours for the taking!")
     else:
-        print("the currently high score is {} attempts".format(min(attempts_list)))
-        def start_game():
-            rondom number =int(rondom.randint(1,10))
-            print("hey there! welcome to the game of gueses!")
-            player_name = input("enter your name")
-            wanna_play = input("hi, {},would you like to play the guessing game?"("enter yes/no". format (play_name)))
-             attempts = 0
-            show_score()
-            while wanna_play.lower() == "yes":
-               try:
-                  guess = input("pick a number between 1to10")
-                  if int (guess) < 1 or int (guess) > 10:
-                     raise valueerror ("please guess a nuber with in given range")
-                  if int(guess) == rondom_number:
-                     print ("Gongrats! you guessed it right!")
-                     attempts += 1
-                     attempts_list.appened(attempts)
-                     print("it took you {} ttempts".format(attempts))
-                     play_again = input("would you like to the game again? (enter yes/no")
-                     attempts = 0
-                     show_score
-                     rondom_number = int (rondom.randint(1, 10))
+        print("The current high score is {} attempts".format(min(attempts_list)))
+def start_game():
+    random_number = int(random.randint(1, 10))
+    print("Hey There! Welcome to the game of guesses!")
+    player_name = input("Enter your name!")
+    wanna_play = input("Hi, {}, would you like to play the guessing game? (Enter Yes/No) ".format(player_name))
+    attempts = 0
+    show_score()
+    while wanna_play.lower() == "yes":
+        try:
+            guess = input("Pick a number between 1 and 10 ")
+            if int(guess) < 1 or int(guess) > 10:
+                raise ValueError("Please guess a number within the given range")
+            if int(guess) == random_number:
+                print("Congrats! You guessed it right!")
+                attempts += 1
+                attempts_list.append(attempts)
+                print("It took you {} attempts".format(attempts))
+                play_again = input("Would you like to play again? (Enter Yes/No) ")
+                attempts = 0
+                show_score()
+                random_number = int(random.randint(1, 10))
+                if play_again.lower() == "no":
+                    print("That's cool, have a nice day!")
+                    break
+            elif int(guess) < random_number:
+                print("It's higherc")
+                attempts += 1
+            elif int(guess) > random_number:
+                print("it's lower")
+                attempts += 1
+        except ValueError as err:
+            print("Oh!, that is not a valid value. Try again...")
+            print("({})".format(err))
+    else:
+        print("That's cool, have a nice day!")
+if __name__ == '__main__':
+    start_game()
